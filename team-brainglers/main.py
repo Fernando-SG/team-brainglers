e522a7e7e082db61794518f3281d57f3b023fcf5
Demo User <demouser@MacBook-Pro.local>
2015-07-27 10:39:47 -0700

    second commit by Caroline

--- team-brainglers/main.py
+++ team-brainglers/main.py
@@ -15,10 +15,26 @@
 # limitations under the License.
 #
 import webapp2
+from google.appengine.api import users
+
+env= jinja2.Environment(loader= jinja2.FileSystemLoader('templates'))
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template=env.get_template('main.html')
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/'))
+
+        self.response.out.write('<html><body>%s</body></html>' % greeting)
+        self.response.write(template.render())
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
